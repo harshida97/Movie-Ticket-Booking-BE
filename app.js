49fa98baee26236b0b959605757e5c2e9975d33c
Harshida <harshidayapally@gmail.com>
2024-10-29 20:33:26 +0530

    vercel.json updated

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "https://movie-ticket-booking-fe-u6oc.vercel.app/",
+   // "http://localhost:5173"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Authorization', 'Content-Type']
